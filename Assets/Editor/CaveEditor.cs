59fb4331d40364fe65f79d19693dba9ed96a4a45
Dechichi01 <gabrieldechichi@gmail.com>
2016-07-27 22:16:01 -0300

    CaveGenerator prototyping

---
+++ Assets/Editor/CaveEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(CaveGenerator))]
+public class CaveEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        CaveGenerator room = target as CaveGenerator;
+        if (DrawDefaultInspector())
+            room.GenerateMap();
+    }
+}
